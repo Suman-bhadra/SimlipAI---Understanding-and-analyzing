4da58a9927fc9369b4ac2a9eb3f6f6fbd0acc767
Suman-bhadra <sbhadra577@gmail.com>
2026-08-08 17:30:42 +0530

    Doing live deployment using vercel and render

--- extension/config.js
+++ extension/config.js
@@ -1,6 +1,6 @@
 // TODO: Replace the line below with your Render deployment URL once deployed
 // Example: const BASE_URL = "https://your-app-name.onrender.com";
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://simlipai-understanding-and-analyzing.onrender.com";
 
 if (typeof module !== "undefined" && module.exports) {
   module.exports = { BASE_URL };
