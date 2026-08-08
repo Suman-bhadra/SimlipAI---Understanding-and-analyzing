32fc8dd2d8fb99bcac618fb96372656dda28e8bc
Suman-bhadra <sbhadra577@gmail.com>
2026-08-08 17:59:37 +0530

    Fixing popup files

--- extension/src/popup/popup.js
+++ extension/src/popup/popup.js
@@ -1,6 +1,6 @@
 console.log("popup.js loaded");
 const selectedText = document.getElementById("selectedText");
-const BASE_URL = window.BASE_URL || "http://localhost:5000";
+const BASE_URL = window.BASE_URL || "https://simlipai-understanding-and-analyzing.onrender.com";
 const analyzeBtn = document.getElementById("analyzeBtn");
 const loading = document.getElementById("loading");
 const result = document.getElementById("result");
